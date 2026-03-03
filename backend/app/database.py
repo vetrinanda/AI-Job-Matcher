d9c571df9030ef4d9064547d0cca589127d7be2e
Nanda Kumar <nk9219382@gmail.com>
2026-03-03 23:11:45 +0530

    feat: Implement database connection setup with SQLAlchemy

---
+++ backend/app/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+import dotenv
+dotenv.load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db
