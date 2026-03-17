d26c97f30497dd9bb86725dbf6a97f7b9fd5c064
Nanda Kumar <148224808+vetrinanda@users.noreply.github.com>
2026-03-17 22:39:39 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,3 +6,5 @@ import dotenv
 dotenv.load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db
+
+#Move it to the. env file if required
